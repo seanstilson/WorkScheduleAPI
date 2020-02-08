5056d11971082071f4a5d97ed8431a2a89707f5a
Sean Stilson <sstilson@kimobility.com>
2020-02-08 14:48:06 -0600

    Minor commit

--- WorkScheduleAPI/Controllers/WorkScheduleController.cs
+++ WorkScheduleAPI/Controllers/WorkScheduleController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WorkScheduleAPI.Entities;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -31,9 +32,9 @@ namespace WorkScheduleAPI.Controllers
         {
         }
 
-        // PUT api/values/5
+        // PUT api/WorkSchedule/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        public void Put(int id, [FromBody]WorkScheduleItem WorkSCheduleItem)
         {
         }
 
